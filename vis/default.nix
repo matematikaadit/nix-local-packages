235baa410031f217280ccc0b4f94c7ebed7751ef
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2015-04-30 22:00:02 +0700

    Remove installFlags and use makeFlags instead in vis

--- vis/default.nix
+++ vis/default.nix
@@ -8,8 +8,8 @@ stdenv.mkDerivation rec {
     rev = "287521c";
     sha256 = "b72fb7d1e5313627e89131e4fc0104edbc93c93115d73e83abdba1357f63dfc2";
   };
+  makeFlags = "VERSION=$(version) PREFIX=$(out)";
   buildInputs = [ ncurses ];
-  installFlags = "PREFIX=$(out)";
   meta = {
     description = "a vim like text editor";
     longDescription = "vis is a highly efficient vim like text editor.";
