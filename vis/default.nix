7695c9e96730b4c6d4e1475781ad366a4fbd775c
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2015-07-01 04:10:37 +0700

    bump vis version

--- vis/default.nix
+++ vis/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchgit, ncurses }:
 
 stdenv.mkDerivation rec {
-  rev = "11129f3";
-  revcount = "429";
+  rev = "860ad58";
+  revcount = "445";
   version = "0.r${revcount}.g${rev}";
   name = "vis-${version}";
   src = fetchgit {
     url = https://github.com/martanne/vis.git;
     inherit rev;
-    sha256 = "b8fb643c7410577d77c495210f6c06d3d89dbaf92d8fec7452925cd2b5eb1e71";
+    sha256 = "42c9a398ef09eda4e7044744fb5b2a7ae2a6cfa2e51608d0151bcda91425a96f";
   };
   makeFlags = "VERSION=$(version) PREFIX=$(out)";
   buildInputs = [ ncurses ];
